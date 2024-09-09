9ff40475d492fb514568d5ca3321445caa65b0e9
IMSEONGBIN506 <bins506@gmail.com>
2024-09-09 14:34:37 +0900

    ().py

---
+++ Class 2/().py
@@ -0,0 +1,19 @@
+x = int(input())
+
+for i in range(x):
+  stack = []
+  a = input()
+  for j in a:
+    if j == '(':
+      stack.append(j)
+    elif j == ')':
+      if stack:
+        stack.pop()
+      else:
+        print('NO')
+        break
+  else:
+    if not stack:
+      print('YES')
+    else:
+      print('NO')
