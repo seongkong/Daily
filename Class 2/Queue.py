b52bedbd8353def62a1495fcd940b5d2f9851750
IMSEONGBIN506 <bins506@gmail.com>
2024-09-09 15:34:00 +0900

    Queue.py

---
+++ Class 2/Queue.py
@@ -0,0 +1,32 @@
+import sys
+
+x = int(sys.stdin.readline().strip())
+
+stack = []
+
+for i in range(x):
+  cmd = sys.stdin.readline().strip().split()
+  if cmd[0] == 'push':
+    stack.append(cmd[1])
+  elif cmd[0] == 'pop':
+    if len(stack) != 0:
+      print(stack.pop(0))
+    else:
+      print(-1)
+  elif cmd[0] == 'size':
+    print(len(stack))
+  elif cmd[0] == 'empty':
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+  elif cmd[0] == 'front':
+    if len(stack) != 0:
+      print(stack[0])
+    else:
+      print(-1)
+  elif cmd[0] == 'back':
+    if len(stack) != 0:
+      print(stack[-1])
+    else:
+      print(-1)
