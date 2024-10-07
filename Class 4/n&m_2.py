408d434ad94141ace8b1ef53456ab3aaeab223e0
IMSEONGBIN506 <bins506@gmail.com>
2024-10-07 15:24:14 +0900

    n&m_2.py

---
+++ Class 4/n&m_2.py
@@ -0,0 +1,15 @@
+def DFS(start):
+  if len(s) == m:
+    print(' '.join(map(str, s)))
+    return
+  
+  for i in range(start, n + 1):
+    if i not in s:
+      s.append(i)
+      DFS(i + 1)
+      s.pop()
+
+
+n, m = map(int, input().split())
+s = []
+DFS(1)
