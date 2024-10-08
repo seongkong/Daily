ea526a4cb1ac655e46148aaf7f3d5d5facd3a0d0
IMSEONGBIN506 <bins506@gmail.com>
2024-10-08 10:47:19 +0900

    C Programming

---
+++ C/Class_1/AB.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+  double num1, num2;
+
+  scanf("%lf %lf", &num1, &num2);
+
+  printf("%.9f", num1 / num2);
+
+  return 0;
+}
