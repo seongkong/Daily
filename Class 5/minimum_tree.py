b6d844cac41e883c85f32635a4b843162962cdff
IMSEONGBIN506 <bins506@gmail.com>
2024-11-06 17:56:12 +0900

    minimum_tree.py

--- Class 5/minimum_tree.py
+++ Class 5/minimum_tree.py
@@ -1,43 +1,32 @@
 import sys
-input = lambda: sys.stdin.readline().rstrip()
-
+input = sys.stdin.readline
 V, E = map(int, input().split())
+edges = []  # 간선 정보를 담을 공간
 
-# Kruskal Algorithm
-# https://techblog-history-younghunjo1.tistory.com/262
-edges = []
-for _ in range(E):
-    A, B, C = map(int, input().split())
-    edges.append((A, B, C))
-edges.sort(key=lambda x: x[2]) # C(Cost)가 적은 것부터 정렬
+for i in range(E):
+    a, b, w = map(int, input().split())
+    edges.append((w, a, b))  # '간선 비용' 기준으로 정렬을 위해 (비용, 정점, 정점) 형태로 저장
 
-
-# Union-Find
-parent = [i for i in range(V+1)]
-
-def get_parent(x):
-    if parent[x] == x:
-        return x
-    parent[x] = get_parent(parent[x]) # get_parent 거슬러 올라가면서 parent[x] 값도 갱신
+def find_parent(x):  # 서로소 집합 부모 확인
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
     return parent[x]
 
-def union_parent(a, b):
-    a = get_parent(a)
-    b = get_parent(b)
-
-    if a < b: # 작은 쪽이 부모가 된다. (한 집합 관계라서 부모가 따로 있는 건 아님)
+def union_parent(a, b):  # 서로소 집합 그룹 연산
+    a = find_parent(a)
+    b = find_parent(b)
+    if a < b:
         parent[b] = a
     else:
-        parent[a] = b        
+        parent[a] = b
 
-def same_parent(a, b):
-    return get_parent(a) == get_parent(b)
+parent = [i for i in range(V+1)]  # 자기 자신을 부모로 갖는 서로소 집합
+edges.sort()  # 간선 비용 기준 올림차순으로 정렬
+result = 0
 
+for w, a, b in edges: 
+    if find_parent(a) != find_parent(b):  # 사이클이 생기지 않는다면
+        union_parent(a, b)  # 간선 연결
+        result += w  # 비용 계산
 
-answer = 0
-for a, b, cost in edges:
-    # cost가 작은 edge부터 하나씩 추가해가면서 같은 부모를 공유하지 않을 때(사이클 없을 때)만 확정
-    if not same_parent(a, b):
-        union_parent(a, b)
-        answer += cost
-print(answer)
+print(result)
