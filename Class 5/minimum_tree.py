e8ba84418c510800641576e1f22ccf7a9fd58bf8
IMSEONGBIN506 <bins506@gmail.com>
2024-11-19 16:46:18 +0900

    최소 스패닝 트리.py

--- Class 5/minimum_tree.py
+++ Class 5/minimum_tree.py
@@ -1,43 +1,32 @@
 import sys
-input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**6)  # 재귀 호출 한도를 늘림
 
-V, E = map(int, input().split())
-
-# Kruskal Algorithm
-# https://techblog-history-younghunjo1.tistory.com/262
-edges = []
-for _ in range(E):
-    A, B, C = map(int, input().split())
-    edges.append((A, B, C))
-edges.sort(key=lambda x: x[2]) # C(Cost)가 적은 것부터 정렬
-
-
-# Union-Find
-parent = [i for i in range(V+1)]
-
-def get_parent(x):
-    if parent[x] == x:
-        return x
-    parent[x] = get_parent(parent[x]) # get_parent 거슬러 올라가면서 parent[x] 값도 갱신
+def find(x):
+    global parent
+    if parent[x] != x:
+        parent[x] = find(parent[x])  # 경로 압축
     return parent[x]
 
-def union_parent(a, b):
-    a = get_parent(a)
-    b = get_parent(b)
-
-    if a < b: # 작은 쪽이 부모가 된다. (한 집합 관계라서 부모가 따로 있는 건 아님)
-        parent[b] = a
+def union(x, y):
+    pX, pY = find(x), find(y)
+    if pX > pY:
+        parent[pX] = pY
     else:
-        parent[a] = b        
+        parent[pY] = pX
 
-def same_parent(a, b):
-    return get_parent(a) == get_parent(b)
+V, E = map(int, sys.stdin.readline().split())
+arr, parent = [], [i for i in range(V)]  # 초기에는 자기 자신을 부모로 설정
 
+for _ in range(E):
+    v1, v2, dist = map(int, sys.stdin.readline().split())
+    arr.append((dist, v1-1, v2-1))
+arr.sort()
 
-answer = 0
-for a, b, cost in edges:
-    # cost가 작은 edge부터 하나씩 추가해가면서 같은 부모를 공유하지 않을 때(사이클 없을 때)만 확정
-    if not same_parent(a, b):
-        union_parent(a, b)
-        answer += cost
-print(answer)
+mst = 0
+for a in arr:
+    if find(a[1]) == find(a[2]):
+        continue
+    union(a[1], a[2])
+    mst += a[0]
+
+print(mst)
